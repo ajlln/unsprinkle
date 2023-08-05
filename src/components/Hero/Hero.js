28165f5c3c52a89831bdebb0d514a6a162a716f6
Alexander <ajallan3563@hotmail.co.uk>
2023-08-05 11:13:26 +0100

    Workshop completed
    Workshop completed (as per walkthrough) 5th August 2023

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,11 +3,31 @@ import styled from 'styled-components/macro';
 
 const Hero = () => {
   return (
-    <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
-    </Wrapper>
-  );
+      <Wrapper>
+          <picture>
+              <source
+                  type="image/avif"
+                  srcSet={`
+                   /images/hero-img.avif 1x,
+                   /images/hero-img@2x.avif 2x,
+                   /images/hero-img@3x.avif 3x,
+                  `}
+              />
+              <source
+                  type="image/jpeg"
+                  srcSet={`
+                   /images/hero-img.jpg 1x,
+                   /images/hero-img@2x.jpg 2x,
+                   /images/hero-img@3x.jpg 3x,
+                  `}
+              />
+              <HeroImage 
+              alt="Main photo (cat on a black background)"
+              src="/images/hero-img.jpg" />
+          </picture>
+          <Swoop alt="" src="/swoop.svg" />
+      </Wrapper>
+  )
 };
 
 const Wrapper = styled.section`
